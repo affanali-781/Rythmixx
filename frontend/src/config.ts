bebdb3b4dddce4973d847dac7147e508f7a98337
Shoaib Aziz <affanali781@gmail.com>
2025-05-11 18:32:00 +0500

    v1.2

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -5,8 +5,7 @@ export interface Config {
   site: { name: string; description: string; themeColor: string; url: string };
   logLevel: keyof typeof LogLevel;
 }
-
-#aaaaa
+//aaaaa
 
 export const config: Config = {
   site: { name: 'Rythmix', description: 'Collaborative Music Production', themeColor: '#090a0b', url: getSiteURL() },
